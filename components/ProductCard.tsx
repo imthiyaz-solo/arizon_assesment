ba9a39cf7f7e4e6fa44f88bff59b5056e33c39f1
Mohamed Imthiyaz <imthiyaz.developer@gmail.com>
2025-06-10 04:25:29 +0530

    Add to cart button uniformity fixed

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const ProductCard = ({ product, onAddToCart }: Props) => {
     return (
-        <div className='rounded-lg p-4 shadow-lg hover:shadow-2xl transition space-y-2'>
+        <div className='rounded-lg p-4 shadow-lg hover:shadow-2xl transition flex flex-col justify-between h-full'>
             <Link href={`/product/${product.id}`}>
                 <img
                     src={product.image}
@@ -18,17 +18,24 @@ const ProductCard = ({ product, onAddToCart }: Props) => {
                     className='w-full h-48 object-contain cursor-pointer'
                 />
             </Link>
-            <h2 className='mt-8 text-sm font-semibold line-clamp-2 text-left'>{product.title}</h2>
-            <p className='text-green-700 font-bold text-left'>${product.price}</p>
-            <div className='flex justify-end md:justify-start mt-2'>
-                <button
-                    onClick={() => onAddToCart(product)}
-                    className='cursor-pointer w-fit md:w-full bg-blue-600 text-white px-6 py-3 rounded-md hover:bg-blue-700 transition'
-                >
-                    Add to Cart
-                </button>
+
+            <div className='flex-grow mt-4 flex flex-col justify-between'>
+                <div>
+                    <h2 className='text-sm font-semibold line-clamp-2 text-left'>{product.title}</h2>
+                    <p className='text-green-700 font-bold text-left mt-2'>${product.price}</p>
+                </div>
+
+                <div className='flex justify-end md:justify-start mt-4'>
+                    <button
+                        onClick={() => onAddToCart(product)}
+                        className='cursor-pointer w-fit md:w-full bg-blue-600 text-white px-6 py-3 rounded-md hover:bg-blue-700 transition'
+                    >
+                        Add to Cart
+                    </button>
+                </div>
             </div>
         </div>
+
     );
 };
 
